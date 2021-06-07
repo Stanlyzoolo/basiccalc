e095343730684e68d9591297414c0d91be3d014f
Stanlyzoolo <stanlyzoolo@gmail.com>
2021-06-07 21:58:39 +0300

    first task

---
+++ exercise.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main () {
+	Somefunc("1+1")
+}
+func Somefunc (s string) int {
+	var result int
+	strSlice := []string{}
+
+	for _, str := range s {
+		strSlice = append(strSlice, string(str))
+	}
+
+	for _, item := range strSlice {
+		digit1, _ := strconv.Atoi(strSlice[0])
+		digit2, _ := strconv.Atoi(strSlice[2])
+		switch item {
+		case "+":
+			result = digit1 + digit2
+		case "-":
+			result = digit1 - digit2
+		}
+	}
+
+	fmt.Println(result)
+	return result
+}

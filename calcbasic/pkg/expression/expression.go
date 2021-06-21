026bd523fcbbb90a2b0db6ed07a0401a5237e5af
Stanlyzoolo <stanlyzoolo@gmail.com>
2021-06-21 20:10:09 +0300

    Calcbasic. structured project

--- calcbasic/pkg/expression/expression.go
+++ calcbasic/pkg/expression/expression.go
@@ -1,55 +1,53 @@
 package expression
 
-import "errors"
-
-const (
-	State0 int = iota
-	State1
-	State2
-	State3
+import (
+	"errors"
 )
 
-type Expression struct {
-	X, Y      int
-	Operation Operate
-	state     int // 0 - New struct, 1 - X, 2 - X and Operation, 3 - X, Y and Operation
-}
+type Action func(int, int) int
 
-var singledigits = map[string]int{
-	"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
-}
-
-type Operate func(int, int) int
-
-var operators = map[string]Operate{
+var Operators = map[string]Action{
 	"+": func(x, y int) int { return x + y },
 	"-": func(x, y int) int { return x - y },
 }
 
-func (exp *Expression) isReady() bool {
-	return exp.state == State3
+type Expression struct {
+	X, Y      int
+	Evaluation Action
+	state     int
+}
+
+const (
+	Initialized int = iota
+	FirstArgument
+	FirstArgWithOperator
+	Ready
+)
+
+func (exp *Expression) IsReady() bool {
+	return exp.state == Ready
 }
 
 func (exp *Expression) SetArgument(arg int) error {
-	if exp.state == 0 {
+	if exp.state == Initialized {
 		exp.X = arg
-		exp.state = 1
+		exp.state = FirstArgument
 		return nil
 	}
 
-	if exp.state == 2 {
+	if exp.state == FirstArgWithOperator {
 		exp.Y = arg
-		exp.state = 3
+		exp.state = Ready
 		return nil
 	}
 
 	return errors.New("unexpected argument")
 }
 
-func (exp *Expression) SetOperator(fn Operate) error {
-	if exp.state == 1 {
-		exp.Operation = fn
-		exp.state = 2
+func (exp *Expression) SetOperator(fn Action) error {
+	if exp.state == FirstArgument {
+		exp.Evaluation = fn
+		exp.state = FirstArgWithOperator
 		return nil
 	}
 
@@ -57,9 +55,9 @@ func (exp *Expression) SetOperator(fn Operate) error {
 }
 
 func (exp *Expression) Evaluate() (int, error) {
-	if exp.state == 3 {
-		exp.X = exp.Operation(exp.X, exp.Y)
-		exp.state = 1
+	if exp.state == Ready {
+		exp.X = exp.Evaluation(exp.X, exp.Y)
+		exp.state = FirstArgument
 		return exp.X, nil
 	}
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Stanlyzoolo/homeworks
 
 go 1.16
+
+require github.com/pkg/errors v0.9.1

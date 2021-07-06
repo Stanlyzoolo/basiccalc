f9e410d631a0714468fe9703069b616636dc4f99
Stanlyzoolo <stanlyzoolo@gmail.com>
2021-07-06 12:27:56 +0300

    Calcbasic. Benchmarks added

--- mathstuff_test.go
+++ mathstuff_test.go
@@ -52,3 +52,25 @@ func TestCalculate(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkEval3(b *testing.B) {
+	input := "2+1"
+
+	for n := 0; n < b.N; n++ {
+		Eval(input)
+	}
+}
+
+func BenchmarkEva10(b *testing.B) {
+	input := "2+1   +1-8"
+	for n := 0; n < b.N; n++ {
+		Eval(input)
+	}
+}
+
+func BenchmarkEval30(b *testing.B) {
+	input := "2+1-1   +8  -4+3 -1+2 +3-8+9+5"
+	for n := 0; n < b.N; n++ {
+		Eval(input)
+	}
+}
